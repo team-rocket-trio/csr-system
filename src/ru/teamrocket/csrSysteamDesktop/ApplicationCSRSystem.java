d978243e4f0a2b1187c9177f63b54cc51162e7dc
Alexander Shreyner <alexander@shreyner.ru>
2016-11-06 20:05:39 +0400

    Create ApplicationCSRSystem, mini change Main and mini ref

---
+++ src/ru/teamrocket/csrSysteamDesktop/ApplicationCSRSystem.java
@@ -0,0 +1,26 @@
+package ru.teamrocket.csrSysteamDesktop;
+
+/**
+ * Created by Alexander Shreyner on 06.11.2016.
+ */
+
+import javafx.application.Application;
+
+abstract class ApplicationCSRSystem extends Application {
+
+    //TODO-Alexander: Добавить глобальный State.
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+    }
+
+    protected static void launchApp(Class<? extends ApplicationCSRSystem> appClass, String[] argv){
+        Application.launch(appClass, argv);
+    }
+}
--- src/ru/teamrocket/csrSysteamDesktop/Main.java
+++ src/ru/teamrocket/csrSysteamDesktop/Main.java
@@ -6,18 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends ApplicationCSRSystem {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("View/layout/RootLayout.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
+
     }
 
-//лучший комментарий в вашей жизни
+    //TODO-kotofeyskaya: Лучший комментарий в вашей жизни.
     public static void main(String[] args) {
-        launch(args);
+        launchApp(Main.class, args);
     }
 }
