70e1ebd41c566d3eafa6e0dee5a9c31881ff58e5
Alexander Shreyner <alexander@shreyner.ru>
2016-11-08 02:21:13 +0400

    add feature change center scene

--- src/main/java/ru/teamrocket/csrSysteamDesktop/Controller/RootController.java
+++ src/main/java/ru/teamrocket/csrSysteamDesktop/Controller/RootController.java
@@ -1,4 +1,47 @@
 package ru.teamrocket.csrSysteamDesktop.Controller;
 
-public class RootController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RootController implements Initializable {
+
+    @FXML
+    private BorderPane borderPane;
+
+    //TODO-Alexander: Снять ID borderPane с главного элемента.
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("location = [" + location + "], resources = [" + resources + "]");
+    }
+
+    @FXML
+    private void handlerOnAddUser(ActionEvent event){
+        setScene("/AddUser.fxml");
+    }
+
+    @FXML
+    private void handlerOnAddOffer(ActionEvent event){
+        setScene("/AddOffer.fxml");
+    }
+
+    private void setScene(String nameResurce){
+        try {
+            Parent parent = FXMLLoader.load(getClass().getResource(nameResurce));
+            borderPane.setCenter(parent);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/ru/teamrocket/csrSysteamDesktop/Main.java
+++ src/main/java/ru/teamrocket/csrSysteamDesktop/Main.java
@@ -1,26 +1,28 @@
 package ru.teamrocket.csrSysteamDesktop;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import ru.teamrocket.csrSysteamDesktop.Controller.RootController;
 
 public class Main extends ApplicationCSRSystem {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
 
         BorderPane root = FXMLLoader.load(getClass().getResource("/layout/RootLayout.fxml"));
-
-        //TODO-Alexander: Вынести отсюда это! Start#1!
-        Parent addUser = FXMLLoader.load(getClass().getResource("/AddUser.fxml"));
-        root.setCenter(addUser);
-        //TODO-Alexander: End#1!
 
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
+
+    }
+
+    @FXML
+    public void stop() {
 
     }
 
