eda579cfe15b84bc747a28eb60baa31cc6f3b084
Alexander Shreyner <alexander@shreyner.ru>
2016-11-08 02:22:35 +0400

    mini ref

--- src/main/java/ru/teamrocket/csrSysteamDesktop/Controller/AddOfferController.java
+++ src/main/java/ru/teamrocket/csrSysteamDesktop/Controller/AddOfferController.java
@@ -1,7 +1,16 @@
 package ru.teamrocket.csrSysteamDesktop.Controller;
 
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by Alexander on 07.11.2016.
  */
-public class AddOfferController {
+public class AddOfferController implements Initializable {
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
--- src/main/java/ru/teamrocket/csrSysteamDesktop/Controller/AddUserController.java
+++ src/main/java/ru/teamrocket/csrSysteamDesktop/Controller/AddUserController.java
@@ -1,7 +1,16 @@
 package ru.teamrocket.csrSysteamDesktop.Controller;
 
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by Alexander on 07.11.2016.
  */
-public class AddUserController {
+public class AddUserController implements Initializable {
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
